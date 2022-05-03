07169d44a84c45e6501bb672c2a8d91e6c926748
Vaibhav Talele <v.talele@zensar.com>
2022-05-03 13:02:23 +0530

    Update

---
+++ src/app/structuraldir/structuraldir.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-structuraldir',
+  templateUrl: './structuraldir.component.html',
+  styleUrls: ['./structuraldir.component.css']
+})
+export class StructuraldirComponent implements OnInit {
+
+  constructor() { }
+
+  fruits:Array<string>=["Apple","Banana","Chikku","PineApple"];
+
+  disp:boolean=true;
+
+  switch=3;
+  
+  ngOnInit() {
+  }
+
+}
